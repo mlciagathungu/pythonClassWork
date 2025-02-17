2b426b28acd5672af0f5b36d348655f3495b2b8b
mlciagathungu <mlciagathungu1022@gmail.com>
2025-02-17 13:09:25 +0300

    Update variable.py

--- variable.py
+++ variable.py
@@ -1,21 +1,23 @@
 
 
-myName = "John"
+myName = "John" # string variable
 print(myName)
-myNum = 12
+myNum = 12 #interger/ number variable
 print(myNum)
-myHeight=5.8
+myHeight=5.8  #float variable
 print(myHeight)
-is_leap = True
+is_leap = True  #boolean variable
 print(is_leap)
 is_leap = False
 print(is_leap)
-fruits = ["apple", "banana", "orange"]
+fruits = ["apple", "banana", "orange"] # list variable
 print(fruits)
-coordinates=( 10, 20)
+coordinates=( 10, 20) #tuple variable
 print(coordinates)
-numbers=range(1,10)
+numbers=range(1,10) # range variable
 print(numbers)
+set_numbers={1,2,3,4,5} # set variable
+print(set_numbers)
 student={
     "full_name": "James",
     "course": "Python",
